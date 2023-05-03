af84d55122a1cb13091f72b8f3618ddceaff7dd7
medinasp <medinasp@gmail.com>
2023-05-02 22:05:28 -0300

    src/App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React from "react";
 import MySideNav from './components/MySideNav'
 import Sidebar from './components/Sidebar'
 import Resume from './components/Resume'
-import GlobalStyle from './styles/global';
+import GlobalStyle from './styles/global'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import Messages from "./pages/Messages";
+import Settings from "./pages/Settings";
+import Revenue from "./pages/Revenue";
+import NewUsers from "./pages/NewUsers";
 
-const App = () => {
+function App() {
   return (
-    <>
+    <Router >
         <MySideNav />
-        <Sidebar />
-        <Resume />
+        <Routes>
+          <Route path='/home' element={<Home/>} />
+          <Route path='/messages' element={<Messages/>} />
+          <Route path='/new-users' element={<NewUsers/>} />
+          <Route path='/revenue' element={<Revenue/>} />
+          <Route path='/settings' element={<Settings/>} />
+        </Routes>
         <GlobalStyle />
-    </>
+    </Router>
   );
 };
 
