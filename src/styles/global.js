952cb4db1b412c4d96026b7a8036d86d54e9c1ee
medinasp <medinasp@gmail.com>
2023-05-03 22:42:15 -0300

    src/styles/global.js

--- src/styles/global.js
+++ src/styles/global.js
@@ -13,13 +13,32 @@ const Global = createGlobalStyle`
   }
 
   .mysidenav {
-    background-color: teal !important;
+    background-color: #f2f2f2 !important;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    color: blue !important;
+    padding: 60px 0px 0px 30px;
   }
 
   .page {
     font-size: 5rem !important;
     text-align: center !important;
+  }
+
+  .nav-item {
+    margin-top: 80px;
+    border-radius: 100px;
+    background-color: black !important;
+    color: #f2f2f2 !important;
+  }
+
+  .nav-item:visited {
+    background-color: red !important;
+  }
+
+  .nav-item:hover {
+    background-color: gray !important;
+    transition: 0.5s;
+    opacity: 0.7;
   }
 
 `;
